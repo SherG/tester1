e57faab701bc0c6a95e6e3dc93a64f3fe0646e94
sherrie gossett <sherrie.g5@gmail.com>
2019-07-05 03:16:54 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,14 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+const observer = lozad(); // lazy loads elements with default selector as ".lozad"
+observer.observe();
+// Initialize library
+lozad('.lozad', {
+    load: function(el) {
+        el.src = el.dataset.src;
+        el.onload = function() 
+        
+    }
+}).observe()
