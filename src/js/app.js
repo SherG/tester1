db5bc2a7e77dffbba246b0d83626db2c38cea93a
sherrie gossett <sherrie.g5@gmail.com>
2019-07-05 03:19:22 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -11,11 +11,4 @@ if (window.netlifyIdentity) {
 
 const observer = lozad(); // lazy loads elements with default selector as ".lozad"
 observer.observe();
-// Initialize library
-lozad('.lozad', {
-    load: function(el) {
-        el.src = el.dataset.src;
-        el.onload = function() 
-        
-    }
-}).observe()
+
